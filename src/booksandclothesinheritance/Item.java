cd2aa097a53040b1e588b6ba79f42858a27be587
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-29 18:33:07 -0700

    Completed SuperClass Item

--- src/booksandclothesinheritance/Item.java
+++ src/booksandclothesinheritance/Item.java
@@ -1,13 +1,30 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * This is the SuperClass for this project
+ * 
  */
 package booksandclothesinheritance;
 
-/**
- *
- * @author frankbernal
- */
 public class Item {
+    // Protected variables can be accessed by child class
+    protected double price;
+    protected int inStock;
     
-}
+    // Constructor
+    Item () {}
+    
+    // Getters
+    public double getPrice()   { return price; }
+    public    int getInStock() { return inStock; }
+    
+    // Setters
+    public double setPrice(double price) {
+        this.price = price;
+        return this.price;
+    }   // End of setPrice
+    
+    public int setInStock(int inStock) {
+        this.inStock = inStock;
+        return this.inStock;
+    }   // End of setInStock
+    
+}   // End of class
